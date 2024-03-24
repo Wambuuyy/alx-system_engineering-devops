a2931af070c679b2978f239e48cad6e42bbdd9f2
Prudence Wambui <wambuiprudy@gmail.com>
2024-03-24 09:40:10 +0300

    instal a package 5

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,15 @@
 # Puppet Manifest for Task 2: Install a package
 
-# Use exec resource to install Flask via pip3
+# Use exec resource to install Werkzeug and Flask via pip3
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.0.3',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.10/site-packages/werkzeug',
+}
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/Flask',
+  creates => '/usr/local/lib/python3.10/site-packages/Flask',
 }
 

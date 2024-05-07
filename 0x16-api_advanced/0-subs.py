09f37d425c42305e9a56ffa9f4826dc1ec453bb0
Wambuuyy <wambuiprudy@gmail.com>
2024-05-07 19:20:08 +0300

    correct arrangement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,21 +8,21 @@ from requests import get
 
 
 def number_of_subscribers(subreddit):
-  """recursive function that queries the Reddit API and returns
-  a list containing the titles of all hot articles for a given
-  subreddit.  if number of results are found for the given subreddit,
-  the function should return None"""
-  url = f"https://www.reddit.com/r/{subreddit}/about.json"
-  headers = {
-    'User-Agent':
-    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 \
-    (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
-  }
-  response = get(url, headers=headers)
-  reddits = response.json()
+    """recursive function that queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given
+    subreddit.  if number of results are found for the given subreddit,
+    the function should return None"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 \
+        (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
+    }
+    response = get(url, headers=headers)
+    reddits = response.json()
 
-try:
-  subscribers = reddits.get('data').get('subscribers')
-  return int(subscribers)
-except:
-  return 0
+    try:
+        subscribers = reddits.get('data').get('subscribers')
+        return int(subscribers)
+    except:
+        return 0

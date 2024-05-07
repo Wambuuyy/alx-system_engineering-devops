cb1222092366887954a766e668c86d5437fedea7
Wambuuyy <wambuiprudy@gmail.com>
2024-05-07 19:53:29 +0300

    correct the raising exceptions

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,20 +8,21 @@ from requests import get
 
 
 def top_ten(subreddit):
-  """
-  Queries the Reddit API and prints the titles of the first 10 hot posts
-  listed for a given subreddit
-  """
-  url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-  headers = {
-  'user-agent' : 
-  }
-  response = get(url, headers=headers, allow_redirects=False)
-  reddits = response.json()
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {
+    'user-agent' : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 \
+                    (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
+    }
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
 
-  try:
-    children = reddits.get('data').get('children')
-    for i in range(10):
-           print(children[i].get('data').get('title'))
-  except:
-    print('None')
+    try:
+        children = reddits.get('data').get('children')
+        for i in range(10):
+            print(children[i].get('data').get('title'))
+    except Exception:
+        print('None')

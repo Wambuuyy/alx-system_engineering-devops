a40589741167b63530034a1f94d4f97e770d3568
Wambuuyy <wambuiprudy@gmail.com>
2024-05-12 08:20:09 +0300

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 """
-created 6:58 pm 7th july 2024
-@author: Prudence Wambui
+1-top-ten
 """
-from json import loads
-from requests import get
+
+import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N)\
-                AppleWebKit/537.36(KHTML, like Gecko)\
-                Chrome/124.0.0.0 Mobile Safari/537.36'
-    }
-    response = get(url, headers=headers, allow_redirects=False)
-    reddits = response.json()
-
-    try:
-        children = reddits.get('data').get('children')
-        for i in range(10):
-            print(children[i].get('data').get('title'))
-    except Exception:
+    """ Print top ten hot posts """
+    if subreddit:
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+        headers = {'User-Agent': 'Chrome'}
+        response = requests.get(url, headers=headers,  allow_redirects=False)
+        if response.status_code == 200:
+            posts = response.json().get('data').get('children')
+            for post in posts:
+                print(post.get('data').get('title'))
         print('None')
